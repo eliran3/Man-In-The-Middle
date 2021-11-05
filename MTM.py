46c125ccca47240203b212d4e91bed6ec07766d2
Eliran <eliranCoding@gmail.com>
2021-11-05 15:28:48 +0200

    Update MTM.py

--- MTM.py
+++ MTM.py
@@ -12,7 +12,7 @@ def main():
     # Getting the default gateway of the network.
     gateway = utils.get_gateway()
     
-    # Performing the first port scan.
+    # Performing port scan.
     perform_port_scan(gateway, '1-60000')
 
     # Performing the MiTM attack activation.
